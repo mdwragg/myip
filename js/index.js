bccde78c30f470c5065ee5ee2e1b33b0496a61d9
Martin Wragg <martin.wragg@ge.com>
2014-05-30 11:16:27 -0700

    first crack at output IP

---
+++ js/index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+var os = require('os'),
+    ifaces=os.networkInterfaces();
+
+  for (var dev in ifaces) {
+    var alias = 0;
+    ifaces[dev].forEach(function(details){
+      if (details.family == 'IPv4') {
+        console.log(dev + (alias ? ':' + alias : ''), details.address);
+        ++alias;
+      }
+    });
+  }
