4a6ba9607c2dcba86cb1123ef627d78f0f965479
Martin Wragg <martin.wragg@ge.com>
2014-05-30 11:20:58 -0700

    Filter out internal addresses

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var os = require('os'),
   for (var dev in ifaces) {
     var alias = 0;
     ifaces[dev].forEach(function(details){
-      if (details.family == 'IPv4') {
+      if (details.family == 'IPv4' && !details.internal) {
         console.log(dev + (alias ? ':' + alias : ''), details.address);
         ++alias;
       }
